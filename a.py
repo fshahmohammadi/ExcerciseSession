3108d14a4800815dc91b85d8ab3fd17d0a623d5d
Farhad Shahmohammadi <farhad.fnr@gmail.com>
2012-02-21 19:08:39 +0100

    Worked

--- a.py
+++ a.py
@@ -1,28 +1,36 @@
 import glob 
 import string
+import os
 
 def FindAll(path):
-	return glob.glob(path + "/*/*");
+	return glob.glob(path + "/*/*.txt");
 
 def IsAnswerN(line):
-	return string.find(line, ": N") != -1;
+	p = string.find(line, ": N");
+	return p != -1;
 
 def ChangeAnswer(fileName, line):
+	print fileName
 	read = open(fileName)
-	write = open(fileName, "w")
+	write = open(fileName + "2", "w")
 	for tmpLine in read:
 		if(line == tmpLine) :
-			write.write(line[0, len(line)-2] + "M")
+			write.write(line[0 : len(line)-2] + "M" + "\n")
 		else :
-			write.write(tmpline)
+			write.write(tmpLine)
+	read.close()
+	write.close()
+	os.rename(fileName+"2", fileName);
 
 def main():
 	all = FindAll("cleaneddata")
 	for file in all:
 		f = open(file)
 		for line in f:
-			if(IsAnswerN(line)):
+			if IsAnswerN(line):
+				f.close();
 				ChangeAnswer(file, line)
+				break;
 	
 main()
 
