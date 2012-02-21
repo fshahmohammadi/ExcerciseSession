0c8046f7eb84e6abb5ca4d42d04477f8ac3dbac7
Farhad Shahmohammadi <farhad.fnr@gmail.com>
2012-02-21 18:09:39 +0100

    ChangeAnswer

---
+++ a.py
@@ -0,0 +1,24 @@
+import glob 
+
+def FindAll(path):
+	return glob.glob(path + "/*/*");
+
+def IsAnswerN(line)
+	return string.find(line, ": N") != -1;
+
+def ChangeAnswer(fileName, line):
+	f = open(file);
+	f[line][len(f[line]-1] = 'M';
+
+def main():
+	all = FindAll("cleaneddata")
+	for file in all:
+		f = open(file)
+		for line in f
+	
+main()
+
+
+
+
+
